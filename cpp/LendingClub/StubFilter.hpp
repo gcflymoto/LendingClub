83bd34ba90b4b4519ce32beda9b81052155a8cb0
Gregory Czajkowski <greg.czajkowski@intel.com>
2014-08-01 17:05:43 -0500

    add missing files

---
+++ cpp/LendingClub/StubFilter.hpp
@@ -0,0 +1,40 @@
+/*
+Created on July 28, 2014
+
+@author:     Gregory Czajkowski
+
+@copyright:  2013 Freedom. All rights reserved.
+
+@license:    Licensed under the Apache License 2.0 http://www.apache.org/licenses/LICENSE-2.0
+
+@contact:    gregczajkowski at yahoo.com
+*/
+
+#ifndef __LC_STUBFILTER_HPP__
+#define __LC_STUBFILTER_HPP__
+
+#include "Filter.hpp"
+#include "Loan.hpp"
+#include "Utilities.hpp"
+
+namespace lc {
+
+    class StubFilter : public Filter {
+    public:
+        //static const std::string sqlite_type;
+        //static const std::string name;
+
+        StubFilter(const Arguments& args, unsigned* current = nullptr) :
+            Filter("Stub", args) {
+        }
+
+        static bool static_apply(const Filter& self, const LCLoan& loan) {
+            return true;
+        }
+        inline bool apply(const LCLoan& loan) {
+            return true;
+        }
+    };
+};
+
+#endif // __LC_STUBFILTER_HPP__
