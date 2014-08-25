0e1396882593e56a8f02ebb8deab28103e21c838
Gregory Czajkowski <greg.czajkowski@intel.com>
2014-08-24 20:19:20 -0700

    add missing source files

---
+++ cpp/LendingClub/Arguments.cpp
@@ -0,0 +1,17 @@
+/*
+Created on July 28, 2014
+
+@author:     Gregory Czajkowski
+
+@copyright:  2013 Freedom. All rights reserved.
+
+@license:    Licensed under the Apache License 2.0 http://www.apache.org/licenses/LICENSE-2.0
+
+@contact:    gregczajkowski at yahoo.com
+*/
+
+#include "Arguments.hpp"
+
+using namespace lc;
+
+Arguments LCArguments::_args;
