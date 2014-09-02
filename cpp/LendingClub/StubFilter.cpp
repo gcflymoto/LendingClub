23460218139deff2fc868c9f9922caa2589e9dea
Gregory Czajkowski <greg.czajkowski@intel.com>
2014-09-01 21:07:28 -0700

    add missing file

---
+++ cpp/LendingClub/StubFilter.cpp
@@ -0,0 +1,18 @@
+/*
+Created on July 28, 2014
+
+@author:     Gregory Czajkowski
+
+@copyright:  2013 Freedom. All rights reserved.
+
+@license:    Licensed under the Apache License 2.0 http://www.apache.org/licenses/LICENSE-2.0
+
+@contact:    gregczajkowski at yahoo.com
+*/
+
+#include "StubFilter.hpp"
+
+using namespace lc;
+
+const LCString StubFilter::name = "State";
+const FilterValueVector* StubFilter::options = nullptr;
